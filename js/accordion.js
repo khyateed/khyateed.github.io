fc02bba8a1dba3fc908e9ec32558b6c03249e099
Desai <khyate@88e9fe5800d5.ant.amazon.com>
2019-09-10 22:20:21 -0400

    photo changes

--- js/accordion.js
+++ js/accordion.js
@@ -1,36 +1,69 @@
-var $titles = $('.title');
+var $title = $('.title');
 var $contents = $('.content');
 var $container = $('.container');
+var $container_stuff = $('.container_stuff');
 
 
+// changes title background color on hover
+$title.on('mouseover', function(){
+	var $currentTitle = $(this);
 
-$titles.on('click', function(){
+	$currentTitle.css("background", "#333");
+	$currentTitle.css("color", "white");
+});
+
+
+$title.on('mouseout', function(){
+	var $currentTitle = $(this);
+	$currentTitle.css("background", "#e2e2e2");
+	$currentTitle.css("color", "#333");
+});
+
+
+// expands titles to show containers
+$title.on('click', function(){
 	
 	var $currentTitle = $(this);
 	var $currentContent = $currentTitle.next();
 
-
 	$contents.slideUp();
+	$title.css("background", "#e2e2e2");
+	$title.css("color", "#333");
+	$container_stuff.slideUp()
 	if(!$currentContent.is(":visible"))
 		{
 			$currentContent.slideDown();
+			$currentTitle.css("background", "#333");
+			$currentTitle.css("color", "white");
 		}
 });
 
+// dims each container background + shows caption
 $container.on('mouseover', function(){
 	var $currentContainer = $(this);
 	var $currentCaption = $currentContainer.find('div');
 	var $currentImg = $currentContainer.find('img');
-
 	$currentImg.css("-webkit-filter", "brightness(50%)");
 	$currentCaption.show();
 });
+
 
 $container.on('mouseout', function(){
 	var $currentContainer = $(this);
 	var $currentCaption = $currentContainer.find('div')
 	var $currentImg = $currentContainer.find('img');
-
 	$currentImg.css("-webkit-filter", "brightness(100%)");
 	$currentCaption.hide();
+});
+
+// expands more contents of each container when clicked
+$container.on('click', function(){
+	var $currentContainer = $(this);
+	var $currentContainerStuff = $currentContainer.next().next().next();
+
+	$container_stuff.slideUp();
+	if(!$currentContainerStuff.is(":visible"))
+		{	
+			$currentContainerStuff.slideDown();
+		}
 });
